b732e12d25db068da9b53a60ee3579291ddf70c9
ArthurPanoyan <arthur.panoyan@polymtl.ca>
2020-09-28 17:33:43 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -5,7 +5,7 @@
 def order(values: list = None) -> bool:
     if values is None:
         # TODO: Demander les valeurs ici
-        values = [input("Entrez...") for]
+        values = [input("Entrez...") for _ in range(10)]
 
     return values == sorted(values)
 
